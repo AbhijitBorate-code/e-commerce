678194fb27bc3fb23d6be41b745f5af2d00490b6
Abhi <apple@Apples-MacBook-Pro.local>
2024-12-27 12:19:24 +0530

    home page updated

--- e-commerce/src/app/home/home.component.ts
+++ e-commerce/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../seller-service/service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private sellerService: ServiceService, private router : Router) {}
 
+  sellerData: any[] = []; 
+
+  ngOnInit() {
+    this.sellerService.getProductList().subscribe(
+      (seller: any) => {
+        if (Array.isArray(seller)) {
+          this.sellerData = seller;
+        } else if (seller) {
+          this.sellerData.push(seller); 
+        }
+      }
+    );
+  }
+  editItem(e: any) {
+    console.log(e)
+
+    this.router.navigate(['add-product-list'], { queryParams: { id: e.id } });
+  } 
+
+  deleteItem(e: any)  {
+
+
+    this.sellerService.deleteProduct(e.id).subscribe((res : any) => {
+      console.log('product deleted successfully');
+      this.ngOnInit();
+    });
+  }
 }
